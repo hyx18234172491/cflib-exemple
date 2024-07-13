6b0e0051c8ec9760c570b963b98a9908c932792b
hyx <hyx18234172491@163.com>
2024-07-13 21:17:12 +0800

    update

--- my-exemple/optimalSR/104-2/code/104.py
+++ my-exemple/optimalSR/104-2/code/104.py
@@ -7,17 +7,17 @@ import utils
 
 logging.basicConfig(level=logging.ERROR)
 
-URI0 = 'radio://0/80/2M/31E7E7E7E7'
+URI0 = 'radio://0/80/2M/53E7E7E7E7'
 
 if __name__ == '__main__':
     # relative_pos
     log_var = {
-        'recvSeq1': 'uint16_t',
-        'recvNum1': 'uint16_t',
-        'compute1num1': 'uint16_t',
-        'compute2num1': 'uint16_t',
+        'recvSeq2': 'uint16_t',
+        'recvNum2': 'uint16_t',
+        'compute1num2': 'uint16_t',
+        'compute2num2': 'uint16_t',
     }
 
     utils.log_ranging(link_uri=URI0, log_cfg_name='Statistic', log_save_path='../data/test.csv',
-                      log_var=log_var, period_in_ms=48, keep_time_in_s=100)
+                      log_var=log_var, period_in_ms=60, keep_time_in_s=100)
 

faa7eacb00637125c26cd148e7e58a0ea7047ac9
hyx <hyx18234172491@163.com>
2024-07-20 20:52:34 +0800

    update

--- my_exemple/optimalSR/pro_105/code/绘制周期不匹配3图合一/柱状图.py
+++ my_exemple/optimalSR/pro_105/code/绘制周期不匹配3图合一/柱状图.py
@@ -1,65 +1,65 @@
-
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# 加载数据
-data_path = '../../data/7-18-一架50-一架70ms-1.csv'  # 替换为你的文件路径
-data = pd.read_csv(data_path)
+from my_exemple.optimalSR.pro_105.code.a_50ms_70ms.distribution_up import getPlotData as Data_50ms_70ms_up
+from my_exemple.optimalSR.pro_105.code.a_50ms_70ms.distribution_down import getPlotData as Data_50ms_70ms_down
 
-# 筛选 logNumber 为 0 的数据
-filtered_data = data[data['logNumber'] == 1]
-filtered_data = filtered_data[(filtered_data['timestamp'] > 30000) & (filtered_data['timestamp'] < 60000)]
-filtered_data['distance_3_to_1'] = filtered_data['distance_3_to_1'] * 100
-# 移除没有更新的数值
-filtered_data = filtered_data[
-    (filtered_data['Statistic.compute2num1'] != filtered_data['Statistic.compute2num1'].shift()) |
-    (filtered_data['Statistic.compute1num1'] != filtered_data['Statistic.compute1num1'].shift())
-]
-filtered_data = filtered_data[
-    ((filtered_data['timestamp'] > 24000) & (filtered_data['timestamp'] < 25500)) |
-    ((filtered_data['timestamp'] > 30000) & (filtered_data['timestamp'] < 31500)) |
-    ((filtered_data['timestamp'] > 41900) & (filtered_data['timestamp'] < 43800)) |
-    ((filtered_data['timestamp'] > 54000) & (filtered_data['timestamp'] < 55800)) |
-    ((filtered_data['timestamp'] > 60100) & (filtered_data['timestamp'] < 61800))
-]
-filtered_data = filtered_data.head(100)
+from my_exemple.optimalSR.pro_105.code.a_40ms_70ms.distribution_up import getPlotData as Data_40ms_70ms_up
+from my_exemple.optimalSR.pro_105.code.a_40ms_70ms.distribution_down import getPlotData as Data_40ms_70ms_down
 
-# 计算误差
-filtered_data['error'] = filtered_data['distance_3_to_1'] - filtered_data['Statistic.dist1']
-
-# 筛选误差小于20的数据
-filtered_data = filtered_data[filtered_data['error'].abs() < 20]
+from my_exemple.optimalSR.pro_105.code.a_30ms_70ms.distribution_up import getPlotData as Data_30ms_70ms_up
+from my_exemple.optimalSR.pro_105.code.a_30ms_70ms.distribution_down import getPlotData as Data_30ms_70ms_down
 
 # 创建三个不同的错误数据集
-error_data_1 = filtered_data['error']
-error_data_2 = filtered_data['error'] * 1.1  # 示例数据，可以根据实际情况替换
-error_data_3 = filtered_data['error'] * 0.9  # 示例数据，可以根据实际情况替换
+error_data_50_70_up = Data_50ms_70ms_up()
+error_data_50_70_down = Data_50ms_70ms_down()
+
+error_data_40_70_up = Data_40ms_70ms_up()
+error_data_40_70_down = Data_40ms_70ms_down()
+
+error_data_30_70_up = Data_30ms_70ms_up()
+error_data_30_70_down = Data_30ms_70ms_down()
+
+error_datasets = [
+    Data_50ms_70ms_up(),
+
+    Data_40ms_70ms_up(),
+    Data_30ms_70ms_up(),
+    Data_50ms_70ms_down(),
+    Data_40ms_70ms_down(),
+
+    Data_30ms_70ms_down()
+]
 
 # 创建子图
-fig, axes = plt.subplots(1, 3, figsize=(10, 18))
+fig, axes = plt.subplots(2, 3, figsize=(10, 6))
+# 绘制每个子图
+for i, ax in enumerate(axes.flat):
+    if i < len(error_datasets):
+        sns.histplot(error_datasets[i]['error'], kde=True, binwidth=1, ax=ax)
+        mean = error_datasets[i]['error'].mean()
+        std = error_datasets[i]['error'].std()
 
-# 绘制第一个误差分布
-sns.histplot(error_data_1, kde=True, color="blue", binwidth=1, ax=axes[0])
-axes[0].set_title('Error Data 1 Distribution')
-axes[0].set_xlabel('Error')
-axes[0].set_ylabel('Frequency')
+        ax.text(0.5, 0.95, f'$\mu={mean:.2f}$\n$\sigma={std:.2f}$',
+                verticalalignment='top', horizontalalignment='center',
+                transform=ax.transAxes, fontsize=20, bbox=dict(facecolor='white', alpha=0.8))
+        # # 获取当前的x轴范围
+        # xlim = ax.get_xlim()
+        # # 生成四个均匀分布的刻度位置
+        # x_ticks = np.linspace(xlim[0], xlim[1], 4)
+        # ax.set_xticks(x_ticks)
+        ax.set_xlabel('')  # 移除横坐标标签
+        ax.set_ylabel('')  # 移除纵坐标标签
+    else:
+        ax.axis('off')  # 如果没有更多的数据集，隐藏多余的子图
 
-# 绘制第二个误差分布
-sns.histplot(error_data_2, kde=True, color="green", binwidth=1, ax=axes[1])
-axes[1].set_title('Error Data 2 Distribution')
-axes[1].set_xlabel('Error')
-axes[1].set_ylabel('Frequency')
+# 在每列的最上方添加标注
+fig.text(0.23, 0.94, '50ms-70ms', ha='center', fontsize=20)
+fig.text(0.54, 0.94, '40ms-70ms', ha='center', fontsize=20)
+fig.text(0.86, 0.94, '30ms-70ms', ha='center', fontsize=20)
 
-# 绘制第三个误差分布
-sns.histplot(error_data_3, kde=True, color="red", binwidth=1, ax=axes[2])
-axes[2].set_title('Error Data 3 Distribution')
-axes[2].set_xlabel('Error')
-axes[2].set_ylabel('Frequency')
-
-# 显示图例
-axes[0].legend(['Error Data 1'])
-axes[1].legend(['Error Data 2'])
-axes[2].legend(['Error Data 3'])
-
-plt.tight_layout()
+fig.text(0.025, 0.70, 'Moving away', va='center', rotation='vertical', fontsize=20)
+fig.text(0.025, 0.26, 'Moving towards', va='center', rotation='vertical', fontsize=20)
+plt.tight_layout(rect=[0.05, 0, 1, 0.95])
 plt.show()
